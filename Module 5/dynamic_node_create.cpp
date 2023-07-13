8ca81bf24d11b6ecb8e82c60d08a1c9cc3593c0c
Mahfuz9999 <114106379+Mahfuz9999@users.noreply.github.com>
2023-07-13 15:30:51 +0600

    13 jul

---
+++ Module 5/dynamic_node_create.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next;
+    
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+
+    }
+
+};
+int main()
+{
+    Node* a =new Node(10);
+    Node* b =new Node(29);
+
+    a->next=b;
+
+    cout<<a->val<<endl;
+    cout<<a->next->val<<endl;
+    
+    return 0;
+}
